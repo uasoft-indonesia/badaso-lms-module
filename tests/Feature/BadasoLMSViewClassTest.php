e0251203859d135ead79c53da00ebc9b6ed4341c
Nabila Khansa <nabila.khansa@gmail.com>
2022-03-01 09:59:12 +0700

    [RED] create tests for class existence

--- tests/Feature/BadasoLMSViewClassTest.php
+++ tests/Feature/BadasoLMSViewClassTest.php
@@ -14,4 +14,18 @@ class BadasoLMSViewClassTest extends TestCase
         // init user login as normal user
         CallLMSHelperTest::handelUserAuthorize($this);
     }
+    
+    public function testClassIsExist()
+    {
+        global $EXISTING_CLASS_ID;
+        $response = CallLMSHelperTest::withAuthorizeBearer($this)->json('GET', CallLMSHelperTest::getUrlApiV1Prefix("/class/{$EXISTING_CLASS_ID}"));
+        $this->assertNotEmpty($response);
+    }
+
+    public function testClassIsNotExist()
+    {
+        global $NON_EXISTING_CLASS_ID;
+        $response = CallLMSHelperTest::withAuthorizeBearer($this)->json('GET', CallLMSHelperTest::getUrlApiV1Prefix("/class/{$NON_EXISTING_CLASS_ID}"));
+        $this->assertEmpty($response);
+    }
 }
