4b1b4406d3074700bbc28873602c5dab3fa5edad
FXDROS <baskorocoolest@gmail.com>
2022-03-01 20:39:24 +0700

    [RED] Create joinClass empty function for ClassController

---
+++ src/Controllers/ClassController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Uasoft\Badaso\Module\LMSModule\Controllers;
+
+use Uasoft\Badaso\Controllers\Controller;
+
+class ClassController extends Controller
+{
+    public function joinClass()
+    {
+        //join class
+    }
+
+}
