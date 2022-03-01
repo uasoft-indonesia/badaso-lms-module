9531e8a271ccf389b6a957b50ff7fa88c5096e93
FXDROS <baskorocoolest@gmail.com>
2022-03-01 19:26:16 +0700

    [RED] Create handleUserAuthorize test helper

---
+++ src/Helpers/CallLMSHelperTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Uasoft\Badaso\Module\LMS\Helpers;
+
+use Tests\TestCase;
+
+class CallLMSHelperTest
+{
+    public static function handleUserAuthorize(TestCase $test_case)
+    {
+
+    }
+
+    //TODO: CREATE START CLASS HELPER
+}
