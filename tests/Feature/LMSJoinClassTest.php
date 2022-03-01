93ac828fb2c106ac31b3fe5b985cae84641e19ed
FXDROS <baskorocoolest@gmail.com>
2022-03-01 19:59:25 +0700

    [RED] Create testClassIsExist

--- tests/Feature/LMSJoinClassTest.php
+++ tests/Feature/LMSJoinClassTest.php
@@ -3,10 +3,15 @@
 namespace Uasoft\Badaso\Module\LMS\Tests\Feature;
 
 use Tests\TestCase;
-use Uasoft\Badaso\Module\LMS\Helpers\CallLMSHelperTest;
 use Uasoft\Badaso\Module\LMSModule\Helpers\Route;
 
 class LMSJoinClassTest extends TestCase
 {
+    public function testClassIsExist()
+    {
+        $response = Route::getController('ClassController@...');
+        $response->assertSuccessful();
 
+        $this->assertNotNull($response);
+    }
 }
