1d45fa1c494499b8c2218870146c5c0095faa4a4
Aldi Naufal Fitrah <aldifitrahx@gmail.com>
2022-03-01 18:22:20 +0700

    [RED] create test when creating class without logging in first

--- tests/Feature/BadasoLMSCerateClassTest.php
+++ tests/Feature/BadasoLMSCerateClassTest.php
@@ -7,6 +7,17 @@ use Uasoft\Badaso\Helpers\CallHelperTest;
 
 class BadasoLMSCreateClassTest extends TestCase
 {
+    public function testCreateClassWithoutLoginExpectResponseStatus401()
+    {
+        $response = $this->postJson('/classes', [
+            'name' => 'PPL with Badaso',
+            'subject' => 'PPL',
+            'room' => '2-2403',
+        ]);
+
+        $response->assertStatus(401);
+    }
+
     public function testCreateClassWithAllFieldsValidExpectResponseStatus201()
     {
         // TODO: log user in first
