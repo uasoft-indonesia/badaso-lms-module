6724e667ee94fe4c67af8262924049d6849c5571
FXDROS <baskorocoolest@gmail.com>
2022-03-04 13:12:15 +0700

    [CHORES] Change testing method

--- tests/Feature/LMSJoinClassTest.php
+++ tests/Feature/LMSJoinClassTest.php
@@ -11,31 +11,7 @@ class LMSJoinClassTest extends TestCase
     public static string $VALID_CLASS_CODE;
     public static string $INVALID_CLASS_CODE;
 
-    public function testClassIsExist()
-    {
-        $response = Route::getController('ClassController@...');
-        $response->assertSuccessful();
 
-        $this->assertNotNull($response);
-    }
-
-    public function testClassCodeValid()
-    {
-        global $VALID_CLASS_CODE;
-        $class = Route::getController('ClassController@...');
-        $class->assertSuccessful();
-
-        $this->assertEquals($VALID_CLASS_CODE, $class['code']);
-    }
-
-    public function testClassCodeInvalid()
-    {
-        global $INVALID_CLASS_CODE;
-        $class = Route::getController('ClassController@...');
-        $class->assertSuccessful();
-
-        $this->assertNotEquals($INVALID_CLASS_CODE, $class['code']);
-    }
 
 
 }
