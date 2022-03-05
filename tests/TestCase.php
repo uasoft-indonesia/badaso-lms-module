16f8522aabe33b8403f8be086d7dcdb4c7ba3e4f
Aldi Naufal Fitrah <aldifitrahx@gmail.com>
2022-03-05 22:45:16 +0700

    [CHORES] manually setup badaso database prefix on base TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -23,6 +23,6 @@ class TestCase extends \Orchestra\Testbench\TestCase
 
     protected function getEnvironmentSetUp($app)
     {
-        // perform environment setup
+        $app['config']->set('badaso.database.prefix', 'badaso_');
     }
 }
