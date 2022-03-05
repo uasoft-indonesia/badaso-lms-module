8de9fc8ffd64080f189318bd5baa9621a8f1950b
Aldi Naufal Fitrah <aldifitrahx@gmail.com>
2022-03-05 22:55:54 +0700

    [CHORES] use refresh database for base TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,10 +2,13 @@
 
 namespace Uasoft\Badaso\Module\LMSModule\Tests;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Uasoft\Badaso\Module\LMSModule\Providers\LMSModuleProvider;
 
 class TestCase extends \Orchestra\Testbench\TestCase
 {
+    use RefreshDatabase;
+
     protected $loadEnvironmentVariables = true;
 
     public function setUp(): void
