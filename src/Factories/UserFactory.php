bbe716de7bc41a1d25648e057e510d6ab128ce40
Aldi Naufal Fitrah <aldifitrahx@gmail.com>
2022-03-08 08:07:17 +0700

    [GREEN] implement create manyToMany relation to user when create course

---
+++ src/Factories/UserFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Uasoft\Badaso\Module\LMSModule\Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Uasoft\Badaso\Module\LMSModule\Models\User;
+
+class UserFactory extends Factory
+{
+    protected $model = User::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        ];
+    }
+}
