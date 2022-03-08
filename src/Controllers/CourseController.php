3b840bdc5dab51ffd553619d1096669a92cc7c51
Aldi Naufal Fitrah <aldifitrahx@gmail.com>
2022-03-08 08:28:31 +0700

    [GREEN] implement db transaction to avoid insertion when request failed

--- src/Controllers/CourseController.php
+++ src/Controllers/CourseController.php
@@ -14,29 +14,44 @@ class CourseController extends Controller
 {
     public function add(Request $request)
     {
-        $user = auth()->user();
-        $course = Course::create([
-            'name' => $request->input('name'),
-            'subject' => $request->input('subject'),
-            'room' => $request->input('room'),
-            'join_code' => Random::generate(6, '0-9A-Z'),
-            'created_by' => $user->id,
-        ]);
-        CourseUser::create([
-            'course_id' => $course->id,
-            'user_id' => $user->id,
-            'role' => CourseUserRole::TEACHER,
-        ]);
-        $request->validate([
-            'name' => 'required|string|max:255',
-            'subject' => 'required|string|max:255',
-            'room' => 'required|string|max:255',
-        ]);
-        return ApiResponse::success([
-            'id' => 1,
-            'name' => $request->input('name'),
-            'subject' => $request->input('subject'),
-            'room' => $request->input('room'),
-        ]);
+        DB::beginTransaction();
+
+        try {
+            $user = auth()->user();
+
+            $course = Course::create([
+                'name' => $request->input('name'),
+                'subject' => $request->input('subject'),
+                'room' => $request->input('room'),
+                'join_code' => Random::generate(6, '0-9A-Z'),
+                'created_by' => $user->id,
+            ]);
+
+            CourseUser::create([
+                'course_id' => $course->id,
+                'user_id' => $user->id,
+                'role' => CourseUserRole::TEACHER,
+            ]);
+
+            $request->validate([
+                'name' => 'required|string|max:255',
+                'subject' => 'required|string|max:255',
+                'room' => 'required|string|max:255',
+            ]);
+
+            DB::commit();
+
+            return ApiResponse::success([
+                'id' => 1,
+                'name' => $request->input('name'),
+                'subject' => $request->input('subject'),
+                'room' => $request->input('room'),
+            ]);
+        } catch (Exception $e) {
+            DB::rollBack();
+            if ($e instanceof ValidationException) {
+                return response('', 422);
+
+        }
     }
 }
