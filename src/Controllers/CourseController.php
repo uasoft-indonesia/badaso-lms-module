0a1cf7815bc731eac056d5620d8a63fb81f1a44b
Aldi Naufal Fitrah <aldifitrahx@gmail.com>
2022-03-08 07:48:37 +0700

    [GREEN] implement insertion into database when create course

--- src/Controllers/CourseController.php
+++ src/Controllers/CourseController.php
@@ -4,10 +4,18 @@ namespace Uasoft\Badaso\Module\LMSModule\Controllers;
 
 use Illuminate\Http\Request;
 use Uasoft\Badaso\Controllers\Controller;
+use Uasoft\Badaso\Module\LMSModule\Models\Course;
 class CourseController extends Controller
 {
     public function add(Request $request)
     {
+        $course = Course::create([
+            'name' => $request->input('name'),
+            'subject' => $request->input('subject'),
+            'room' => $request->input('room'),
+            'join_code' => Random::generate(6, '0-9A-Z'),
+            'created_by' => $user->id,
+        ]);
         return ApiResponse::success([
             'id' => 1,
             'name' => $request->input('name'),
---
+++ src/Models/User.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Uasoft\Badaso\Module\LMSModule\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Uasoft\Badaso\Models\User as BadasoUser;
+use Uasoft\Badaso\Module\LMSModule\Database\Factories\UserFactory;
+use Uasoft\Badaso\Module\LMSModule\Models\Course;
+
+class User extends BadasoUser
+{
+}
