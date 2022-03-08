9c81f00e8fcdaef70674f1562544c081481995a3
AldiNFitrah <52171676+AldiNFitrah@users.noreply.github.com>
2022-03-08 12:44:23 +0700

    PBI-4: Users can create course (#19)
    * [RED] create test to create course without login
    
    * [GREEN] implement endpoint and handler to create course
    
    * [RED] create test when create course with valid data
    expect response status 200
    
    * [GREEN] response course creation with ApiResponse success
    
    * [RED] create test to when create course with logged in user
    expect response with created course
    
    * [GREEN] implement create course response with request data
    
    * [RED] create test when create course
    expect course inserted  into database
    
    * [GREEN] implement insertion into database when create course
    
    * [RED] create test when create course with valid data
    expect many to many relation created to user with role
    
    * [GREEN] implement create manyToMany relation to user when create course
    
    * [RED] create test when create course with invalid data
    expect response 422
    
    * [GREEN] implement request validation when create course
    
    * [RED] create test when create course with invalid data
    expect no course or courseUser created
    
    * [GREEN] implement db transaction to avoid insertion when request failed
    
    * [CHORE] setting up factories and seeders
    
    * [REFACTOR] apply badaso pattern when handling error
    
    * [REFACTOR] utilize AuthHelper to ease user auth in tests

---
+++ tests/Helpers/AuthHelper.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Uasoft\Badaso\Module\LMSModule\Tests\Helpers;
+
+use Exception;
+use Tests\TestCase;
+
+class AuthHelper
+{
+    protected static $loginUrl = '/admin/v1/auth/login';
+
+    protected static $cache = [];
+
+    public static function asUser(TestCase $testCase, $user)
+    {
+        if (!isset($user->rawPassword)) {
+            throw new Exception(
+                'Please set user raw password first, e.g. `$user->rawPassword = \'password\';`'
+            );
+        }
+
+        $cachedToken = self::getCachedUserToken($user);
+        if (!empty($cachedToken)) {
+            return $cachedToken;
+        }
+
+        $credentials = [
+            'email' => $user->email,
+            'password' => $user->rawPassword,
+        ];
+
+        $loginResponse = $testCase->json('POST', self::$loginUrl, $credentials);
+
+        $token = $loginResponse->json('accessToken');
+
+        self::setCachedUserToken($user, $token);
+
+        return $testCase->withHeader('Authorization', 'Bearer ' . $token);
+    }
+
+    public static function getCacheKey($user)
+    {
+        return $user->email . ' : ' . $user->id;
+    }
+
+    public static function getCachedUserToken($user)
+    {
+        $cacheKey = static::getCacheKey($user);
+        return self::$cache[$cacheKey] ?? NULL;
+    }
+
+    public static function setCachedUserToken($user, $token)
+    {
+        $cacheKey = static::getCacheKey($user);
+        self::$cache[$cacheKey] = $token;
+    }
+}
