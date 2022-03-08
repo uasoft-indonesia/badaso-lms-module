ca77bcea9d472477553c19a3448182b132a0b59a
Muhammad Fathan Muthahhari <fathanmuthahhari@gmail.com>
2022-03-08 15:59:55 +0700

    [GREEN] implement user controller and registration

--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -2,15 +2,54 @@
 
 namespace Uasoft\Badaso\Module\LMSModule\Controllers;
 
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\ValidationException;
 use Uasoft\Badaso\Controllers\Controller;
 use Uasoft\Badaso\Helpers\ApiResponse;
+use Uasoft\Badaso\Module\LMSModule\Models\User;
+use Illuminate\Support\Facades\Hash;
 
-class UserController extends Controller 
+
+class UserController extends Controller
 {
-    public function home()
+    public function register(Request $request)
     {
-        $data['body'] = "hello you are in api";
 
-        return ApiResponse::success($data);
+        DB::beginTransaction();
+
+        try {
+            $request->validate([
+                'name' => 'required|string|max:255',
+                'email' => 'required|string|email',
+                'username' => 'required|string|max:255',
+                'password' => 'required|string|max:255',
+                'password_confirmation' => 'required|string|max:255',
+            ]);
+
+            if ($request->input('password') != $request->input('password_confirmation')) {
+                return ApiResponse::failed('Password does not match, please try again');
+            }
+
+            $user = User::factory()->create([
+                'name' => $request->input('name'),
+                'email' => $request->input('email'),
+                'username' => $request->input('username'),
+                'password' => Hash::make($request->input('password')),
+            ]);
+
+            DB::commit();
+
+            return ApiResponse::success($user);
+        } catch (Exception $e) {
+            DB::rollBack();
+
+            if($e instanceof ValidationException) {
+                return ApiResponse::failed($e);
+            }
+
+            return ApiResponse::failed('Failed to register, please try again');
+        }
     }
 }
