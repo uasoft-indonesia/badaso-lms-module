83950c75b35185134778cdc5c1ba32ad65b47fd5
nofamex <nsiren517@gmail.com>
2022-03-09 03:00:50 +0700

    [GREEN] Implement api for login

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -14,6 +14,38 @@ class AuthController extends Controller
 {
     public function login(Request $request)
     {
-        
+        try {
+            $remember = $request->get('remember', false);
+            $credentials = [
+                'email'    => $request->email,
+                'password' => $request->password,
+            ];
+            $request->validate([
+                'email' => ['required'],
+                'password' => ['required'],
+            ]);
+
+            $token = Auth::attempt($credentials);
+
+            if ($token) {
+                return $this->createNewToken($token, Auth::user(), $remember);
+            }
+
+            return ApiResponse::failed("authentication failed");
+        } catch (JWTException $e) {
+            return ApiResponse::failed($e);
+        } catch (Exception $e) {
+            return ApiResponse::failed($e);
+        }
+    }
+
+    protected function createNewToken($token, $user, $remember = false)
+    {
+        $obj = new stdClass();
+        $obj->access_token = $token;
+        $obj->token_type = 'bearer';
+        $obj->user = $user;
+
+        return ApiResponse::success($obj);
     }
 }
