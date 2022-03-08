8dfc94781d6c86b4c27f4d1321c1aa46eb8da735
Work Place <57487697+FXDROS@users.noreply.github.com>
2022-03-09 00:56:31 +0700

    [RED] Create test to check if user is not authorized

--- tests/Feature/LMSJoinClassTest.php
+++ tests/Feature/LMSJoinClassTest.php
@@ -8,24 +8,12 @@ use Uasoft\Badaso\Module\LMSModule\Tests\TestCase;
 class LMSJoinClassTest extends TestCase
 {
 
-    public static string $VALID_CLASS_CODE = '12345';
-    public static string $INVALID_CLASS_CODE;
-    public static int $TEST_COURSE_ID = 1;
-
-    public function testJoinClassWithoutLoginExpectResponseStatus401()
-    {
-        global $VALID_CLASS_CODE;
-        global $TEST_COURSE_ID;
-
-        $course = $this->getJson("/course/{$TEST_COURSE_ID}");
-
-        $response = $this->postJson('/courses/join', [
-            'id' => $course['id'],
-            'code' => $VALID_CLASS_CODE,
-        ]);
-
-        $response->assertStatus(401);
-    }
+  public function testJoinClassWithoutLoginExpectResponseStatus401()
+  {
+    $url = route('badaso.course.join');
+    $response = $this->json('POST', $url);
+    $response->assertStatus(401);
+  }
 
 
 }
