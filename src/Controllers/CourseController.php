b1efd113f246ed7a4f2ee1e365a98dd58d747746
Aldi Naufal Fitrah <aldifitrahx@gmail.com>
2022-03-08 07:36:54 +0700

    [GREEN] implement endpoint and handler to create course

---
+++ src/Controllers/CourseController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Uasoft\Badaso\Module\LMSModule\Controllers;
+
+use Illuminate\Http\Request;
+use Uasoft\Badaso\Controllers\Controller;
+class CourseController extends Controller
+{
+    public function add(Request $request)
+    {
+    }
+}
--- src/Routes/api.php
+++ src/Routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Uasoft\Badaso\Middleware\ApiRequest;
+use Uasoft\Badaso\Middleware\BadasoAuthenticate;
 use Uasoft\Badaso\Module\LMSModule\Helpers\Route as HelpersRoute;
 
 $api_route_prefix = config('Badaso.api_route_prefix', 'badaso-api');
@@ -10,6 +11,12 @@ Route::group(['prefix' => $api_route_prefix, 'as' => 'badaso.', 'middleware' => 
     Route::group(['prefix' => 'module/lms/v1'], function() {
         Route::group(['prefix' => 'user'], function() {
             Route::get('/home', HelpersRoute::getController('UserController@home'));
+        });
+
+        Route::group(['prefix' => 'course', 'as' => 'course.'], function() {
+            Route::post('/', HelpersRoute::getController('CourseController@add'))
+                ->middleware(BadasoAuthenticate::class)
+                ->name('add');
         });
     });
 });
