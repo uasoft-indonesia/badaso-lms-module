3632e60c30eb0f9f021d2171f6d48282677d55d8
Work Place <57487697+FXDROS@users.noreply.github.com>
2022-03-09 15:11:36 +0700

    [REFACTOR] Reformat space indentation

--- tests/Feature/LMSJoinClassTest.php
+++ tests/Feature/LMSJoinClassTest.php
@@ -12,81 +12,81 @@ use Uasoft\Badaso\Module\LMSModule\Tests\Helpers\AuthHelper;
 class LMSJoinClassTest extends TestCase
 {
 
-  public function testJoinClassWithoutLoginExpectResponseStatus401()
-  {
-    $url = route('badaso.course.join');
-    $response = $this->json('POST', $url);
-    $response->assertStatus(401);
-  }
+    public function testJoinClassWithoutLoginExpectResponseStatus401()
+    {
+        $url = route('badaso.course.join');
+        $response = $this->json('POST', $url);
+        $response->assertStatus(401);
+    }
 
-  public function testJoinClassAsAuthorizedUserWithUnknownClassCodeExpectResponseStatus404()
-  {
-    $user = User::factory()->create();
-    $user->rawPassword = 'password';
+    public function testJoinClassAsAuthorizedUserWithUnknownClassCodeExpectResponseStatus404()
+    {
+        $user = User::factory()->create();
+        $user->rawPassword = 'password';
 
-    $url = route('badaso.course.join');
+        $url = route('badaso.course.join');
 
-    $response = AuthHelper::asUser($this, $user)->json('POST', $url, [
-      'code' => 'xxx'
-    ]);
-    $response->assertStatus(404);
-  }
+        $response = AuthHelper::asUser($this, $user)->json('POST', $url, [
+            'code' => 'xxx'
+        ]);
+        $response->assertStatus(404);
+    }
 
-  public function testJoinClassAsAuthorizedUserWithNoClassCodeAsInputExpectResponseStatus400()
-  {
-    $user = User::factory()->create();
-    $user->rawPassword = 'password';
+    public function testJoinClassAsAuthorizedUserWithNoClassCodeAsInputExpectResponseStatus400()
+    {
+        $user = User::factory()->create();
+        $user->rawPassword = 'password';
 
-    $url = route('badaso.course.join');
+        $url = route('badaso.course.join');
 
-    $response = AuthHelper::asUser($this, $user)->json('POST', $url, []);
-    $response->assertStatus(400);
-  }
+        $response = AuthHelper::asUser($this, $user)->json('POST', $url, []);
+        $response->assertStatus(400);
+    }
 
-  public function testJoinClassAsAuthorizedUserWithValidClassCodeExpectResponseStatus200()
-  {
-    $userTeacher = User::factory()->create();
-    $userTeacher->rawPassword = 'password';
+    public function testJoinClassAsAuthorizedUserWithValidClassCodeExpectResponseStatus200()
+    {
+        $userTeacher = User::factory()->create();
+        $userTeacher->rawPassword = 'password';
 
-    $userStudent = User::factory()->create();
-    $userStudent->rawPassword = 'password';
+        $userStudent = User::factory()->create();
+        $userStudent->rawPassword = 'password';
 
-    $course = Course::factory()->create(['created_by' => $userTeacher->id]);
+        $course = Course::factory()->create(['created_by' => $userTeacher->id]);
 
-    $joinUrl = route('badaso.course.join');
-    $response = AuthHelper::asUser($this, $userStudent)->json('POST', $joinUrl, [
-      'code' => $course->join_code,
-    ]);
+        $joinUrl = route('badaso.course.join');
+        $response = AuthHelper::asUser($this, $userStudent)->json('POST', $joinUrl, [
+            'code' => $course->join_code,
+        ]);
 
-    $response->assertStatus(200);
-  }
+        $response->assertStatus(200);
+    }
 
-  public function testJoinClassAsAuthorizeUserWithValidClassCodeShouldAddStudent()
-  {
-    $userTeacher = User::factory()->create();
-    $userTeacher->rawPassword = 'password';
+    public function testJoinClassAsAuthorizeUserWithValidClassCodeShouldAddStudent()
+    {
+        $userTeacher = User::factory()->create();
+        $userTeacher->rawPassword = 'password';
 
-    $userStudent = User::factory()->create();
-    $userStudent->rawPassword = 'password';
+        $userStudent = User::factory()->create();
+        $userStudent->rawPassword = 'password';
 
-    $course = Course::factory()->create(['created_by' => $userTeacher->id]);
+        $course = Course::factory()->create(['created_by' => $userTeacher->id]);
 
-    $courseUserBeforeCount = CourseUser::where(
-      'course_id', $course->id)
-      ->where('role', 'student')
-      ->count();
+        $courseUserBeforeCount = CourseUser::where(
+            'course_id', $course->id)
+            ->where('role', 'student')
+            ->count();
 
-    $joinUrl = route('badaso.course.join');
-    AuthHelper::asUser($this, $userStudent)->json('POST', $joinUrl, [
-      'code' => $course->join_code,
-    ]);
+        $joinUrl = route('badaso.course.join');
+        AuthHelper::asUser($this, $userStudent)->json('POST', $joinUrl, [
+            'code' => $course->join_code,
+        ]);
 
-    $courseUserAfterCount = CourseUser::where(
-      'course_id', $course->id)
-      ->where('role', 'student')
-      ->count();
+        $courseUserAfterCount = CourseUser::where(
+            'course_id', $course->id)
+            ->where('role', 'student')
+            ->count();
 
-    $this->assertEquals(0, $courseUserBeforeCount);
-    $this->assertEquals(1, $courseUserAfterCount);
-  }
+        $this->assertEquals(0, $courseUserBeforeCount);
+        $this->assertEquals(1, $courseUserAfterCount);
+    }
 }
