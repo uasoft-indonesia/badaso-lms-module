cd3c6ac43ccdd39b093d2083b701677f341e9c43
Work Place <57487697+FXDROS@users.noreply.github.com>
2022-03-09 10:49:12 +0700

    [GREEN] Implement join class controller

--- src/Controllers/CourseController.php
+++ src/Controllers/CourseController.php
@@ -2,7 +2,12 @@
 
 namespace Uasoft\Badaso\Module\LMSModule\Controllers;
 
+use Doctrine\DBAL\Exception\ConstraintViolationException;
+use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
 use Exception;
+use Facade\FlareClient\Http\Exceptions\NotFound;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Validation\ValidationException;
@@ -12,6 +17,7 @@ use Uasoft\Badaso\Helpers\ApiResponse;
 use Uasoft\Badaso\Module\LMSModule\Enums\CourseUserRole;
 use Uasoft\Badaso\Module\LMSModule\Models\Course;
 use Uasoft\Badaso\Module\LMSModule\Models\CourseUser;
+use UniSharp\LaravelFilemanager\Exceptions\DuplicateFileNameException;
 
 class CourseController extends Controller
 {
@@ -53,6 +59,44 @@ class CourseController extends Controller
             }
 
             return ApiResponse::failed('Failed to create course, please try again');
+        }
+    }
+
+    public function join(Request $request)
+    {
+        DB::beginTransaction();
+
+        try {
+            $user = auth()->user();
+
+            $request->validate([
+                'code' => 'required|string|max:255',
+            ]);
+
+            $course = Course::where(
+                'join_code', '=', $request->input('code')
+            )->firstOrFail();
+
+            $courseUser = CourseUser::create([
+                'course_id' => $course->id,
+                'user_id' => $user->id,
+                'role' => CourseUserRole::STUDENT,
+            ]);
+
+            DB::commit();
+
+            return ApiResponse::success($courseUser);
+        } catch (Exception $e){
+            DB::rollBack();
+
+            if ($e instanceof ValidationException){
+                return ApiResponse::failed($e);
+            } else if ($e instanceof ModelNotFoundException){
+                return ApiResponse::failed('Class code is invalid');
+            } else if ($e instanceof QueryException){
+                return ApiResponse::failed('You have been registered in this class already');
+            }
+            return ApiResponse::failed('Failed to join class, please try again');
         }
     }
 }
