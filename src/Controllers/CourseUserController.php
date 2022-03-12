d0622327a050310b7910fd162f4c2597cebf454e
Nabila Khansa <nabila.khansa@gmail.com>
2022-03-12 14:47:08 +0700

    [GREEN] implement view api

--- src/Controllers/CourseUserController.php
+++ src/Controllers/CourseUserController.php
@@ -7,21 +7,41 @@ use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
 use Uasoft\Badaso\Controllers\Controller;
 use Uasoft\Badaso\Helpers\ApiResponse;
+
+use Uasoft\Badaso\Module\LMSModule\Models\User;
+use Uasoft\Badaso\Module\LMSModule\Models\Course;
 use Uasoft\Badaso\Module\LMSModule\Models\CourseUser;
 
 class CourseUserController extends Controller
 {
-    public function view(Request $request)
+    public function view()
     {
         try {
             $user = auth()->user();
 
             $courseUsers = CourseUser::where(
                 'user_id', '=', $user->id
-            )->get();
+            )->pluck('course_id')->toArray();
 
+            $courses = [];
 
-            return ApiResponse::success($courseUsers);
+            foreach ($courseUsers as $cid) 
+            {
+                $course = Course::find($cid);
+                array_push($courses, $course);
+            }
+
+            foreach ($courses as $crs)
+            {
+                $teacher = User::find(
+                    $crs->createdBy
+                )->pluck('name')->toArray();
+                
+                $crs->created_by = $teacher[0];
+            }
+            
+            // return $courses;
+            return ApiResponse::success($courses);
         } catch (Exception $e) {
             if ($e instanceof ValidationException){
                 
