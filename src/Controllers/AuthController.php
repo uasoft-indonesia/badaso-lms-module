5b3f73a90831bec5d8f0541c6965494deadac4c1
Muhammad Fathan Muthahhari <fathanmuthahhari@gmail.com>
2022-03-13 21:11:59 +0700

    PBI 1 register (#21)
    * feat: add sonarqube integration
    
    * fix: sonarqube integration
    
    * fix: sonarqube integration
    
    * fix: sonarqube directory
    
    * fix: sonarqube
    
    * feat: add sonarconfig
    
    * Apply fixes from StyleCI
    
    * [RED] create test for register
    
    * fix conflicts
    
    * [RED] add test register api
    
    * [RED] add failed user registration test
    
    * [CHORES] fix styling to match convention
    
    * [GREEN] implement user controller and registration
    
    * [REFACTOR] change controller name for auth
    
    * [REFACTOR] change routes name for auth
    
    * [REFACTOR] fix styling to pass styleci
    
    * [REFACTOR] fix styling to pass styleci
    
    * [REFACTOR] fix styling to pass styleci
    
    * [REFACTOR] add newline
    
    * [REFACTOR] change name and remove comments
    
    * [REFACTOR] remove delete user on test
    
    * [REFACTOR] use model create instead of factory on controller
    
    Co-authored-by: Nofaldi Fikrul Atmam <nsiren517@gmail.com>
    Co-authored-by: StyleCI Bot <bot@styleci.io>
    Co-authored-by: Rizki Heryandi <rizkiheryandi@gmail.com>

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -5,13 +5,55 @@ namespace Uasoft\Badaso\Module\LMSModule\Controllers;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 use Tymon\JWTAuth\Exceptions\JWTException;
 use Uasoft\Badaso\Controllers\Controller;
 use Uasoft\Badaso\Helpers\ApiResponse;
 use Uasoft\Badaso\Module\LMSModule\Helpers\Token;
+use Uasoft\Badaso\Module\LMSModule\Models\User;
 
 class AuthController extends Controller
 {
+    public function register(Request $request)
+    {
+        DB::beginTransaction();
+
+        try {
+            $request->validate([
+                'name' => 'required|string|max:255',
+                'email' => 'required|string|email',
+                'username' => 'required|string|max:255',
+                'password' => 'required|string|max:255',
+                'password_confirmation' => 'required|string|max:255',
+            ]);
+
+            if ($request->input('password') != $request->input('password_confirmation')) {
+                return ApiResponse::failed('Password does not match, please try again');
+            }
+
+            $user = User::create([
+                'name' => $request->input('name'),
+                'email' => $request->input('email'),
+                'username' => $request->input('username'),
+                'password' => Hash::make($request->input('password')),
+            ]);
+
+            DB::commit();
+
+            return ApiResponse::success($user);
+        } catch (Exception $e) {
+            DB::rollBack();
+
+            if ($e instanceof ValidationException) {
+                return ApiResponse::failed($e);
+            }
+
+            return ApiResponse::failed('Failed to register, please try again');
+        }
+    }
+
     public function login(Request $request)
     {
         try {
--- src/Routes/api.php
+++ src/Routes/api.php
@@ -12,6 +12,8 @@ Route::group(['prefix' => $api_route_prefix, 'as' => 'badaso.', 'middleware' => 
         Route::group(['prefix' => 'auth', 'as' => 'auth.'], function () {
             Route::post('/login', HelpersRoute::getController('AuthController@login'))
                 ->name('login');
+            Route::post('/register', HelpersRoute::getController('AuthController@register'))
+                ->name('register');
         });
 
         Route::group(['prefix' => 'course', 'as' => 'course.'], function () {
