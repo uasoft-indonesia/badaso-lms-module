1c47630800ff5f6ae6f87dca32f8e51b4e9ba6af
Aldi Naufal Fitrah <aldifitrahx@gmail.com>
2022-03-25 14:40:38 +0700

    [RED] test create announcement with no content

--- tests/Feature/announcement/AnnouncementApiTest.php
+++ tests/Feature/announcement/AnnouncementApiTest.php
@@ -3,6 +3,8 @@
 namespace Uasoft\Badaso\Module\LMSModule\Tests\Feature;
 
 use Tests\TestCase;
+use Uasoft\Badaso\Module\LMSModule\Enums\CourseUserRole;
+use Uasoft\Badaso\Module\LMSModule\Models\Course;
 use Uasoft\Badaso\Module\LMSModule\Models\User;
 use Uasoft\Badaso\Module\LMSModule\Tests\Helpers\AuthHelper;
 
@@ -24,6 +26,23 @@ class AnnonuncementApiTest extends TestCase
         $response = AuthHelper::asUser($this, $user)->json('POST', $url, [
             'course_id' => 1,
             'content' => 'Test content',
+        ]);
+
+        $response->assertStatus(400);
+    }
+
+    public function testCreateAnnouncementInEnrolledCourseWithNoContentExpectResponse400()
+    {
+        $user = User::factory()->create();
+        $user->rawPassword = 'password';
+
+        $course = Course::factory()
+            ->hasAttached($user, ['role' => CourseUserRole::TEACHER])
+            ->create();
+
+        $url = route('badaso.announcement.add');
+        $response = AuthHelper::asUser($this, $user)->json('POST', $url, [
+            'course_id' => $course->id,
         ]);
 
         $response->assertStatus(400);
