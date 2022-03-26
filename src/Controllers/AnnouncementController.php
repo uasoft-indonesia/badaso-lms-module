d6e58c655622846bafffd8d6ee2eb594469b026f
AldiNFitrah <52171676+AldiNFitrah@users.noreply.github.com>
2022-03-26 18:09:21 +0700

    PBI-9: create announcement (#37)
    * [RED] test create announcement should response 401 when user is not logged in
    
    * [GREEN] implement create announcement endpoint skeleton
    
    * [RED] test create announcement to unenrolled course
    
    * [GREEN] implement create announcement course_id validation
    
    * [RED] test create announcement with no content
    
    * [GREEN] implement create announcement content validation
    
    * [RED] test create announcement should insert data to database
    
    * [GREEN] implement insert data to database on create announcement
    
    * [RED] test create announcement should return created announcement
    
    * [GREEN] return announcement data on success creation
    
    * [CHORE] fix styleCI
    
    * [REFACTOR] change content limit to text default instead of 255

---
+++ src/Controllers/AnnouncementController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Uasoft\Badaso\Module\LMSModule\Controllers;
+
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use Uasoft\Badaso\Controllers\Controller;
+use Uasoft\Badaso\Helpers\ApiResponse;
+use Uasoft\Badaso\Module\LMSModule\Helpers\CourseUserHelper;
+use Uasoft\Badaso\Module\LMSModule\Models\Announcement;
+
+class AnnouncementController extends Controller
+{
+    public function add(Request $request)
+    {
+        try {
+            $user = auth()->user();
+            $request->validate([
+                'course_id' => 'required|integer',
+                'content' => 'required|string|max:65535',
+            ]);
+
+            if (! CourseUserHelper::isUserInCourse($user->id, $request->input('course_id'))) {
+                throw ValidationException::withMessages([
+                    'course_id' => 'Course not found',
+                ]);
+            }
+
+            $announcement = Announcement::create([
+                'course_id' => $request->input('course_id'),
+                'content' => $request->input('content'),
+                'created_by' => $user->id,
+            ]);
+
+            return ApiResponse::success($announcement);
+        } catch (Exception $e) {
+            return ApiResponse::failed($e);
+        }
+    }
+}
