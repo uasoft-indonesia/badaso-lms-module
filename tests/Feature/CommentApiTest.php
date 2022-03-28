6a1a4b02f8c96838d9830314297c9521d789ed4a
Nabila Khansa <nabila.khansa@gmail.com>
2022-03-28 20:17:23 +0700

    [RED] tests created comment is properly saved and returned

--- tests/Feature/CommentApiTest.php
+++ tests/Feature/CommentApiTest.php
@@ -3,10 +3,14 @@
 namespace Uasoft\Badaso\Module\LMSModule\Tests\Feature;
 
 use Tests\TestCase;
+use Uasoft\Badaso\Module\LMSModule\Enums\CourseUserRole;
+use Uasoft\Badaso\Module\LMSModule\Models\Announcement;
+use Uasoft\Badaso\Module\LMSModule\Models\Comment;
+use Uasoft\Badaso\Module\LMSModule\Models\Course;
 use Uasoft\Badaso\Module\LMSModule\Models\User;
 use Uasoft\Badaso\Module\LMSModule\Tests\Helpers\AuthHelper;
 
-class ViewApiTest extends TestCase
+class CommentApiTest extends TestCase
 {
     public function testCreateCommentWithoutLoginExpectResponse401()
     {
@@ -27,5 +31,64 @@ class ViewApiTest extends TestCase
         ]);
 
         $response->assertStatus(400);
+    }
+
+    public function testCreateCommentInExistingAnnouncementWithNoContentExpectResponse400()
+    {
+        $user = User::factory()->create();
+        $user->rawPassword = 'password';
+
+        $url = $url = route('badaso.announcement.comment');
+        $response = AuthHelper::asUser($this, $user)->json('POST', $url, [
+            'announcement_id' => 1
+        ]);
+
+        $response->assertStatus(400);
+    }
+
+    public function testCreateCommentInExistingAnnouncementWithValidDataExpectInsertedToDatabase()
+    {
+        $user = User::factory()->create();
+        $user->rawPassword = 'password';
+
+        $announcement = Announcement::factory()->create();
+
+        $url = $url = route('badaso.announcement.comment');
+        AuthHelper::asUser($this, $user)->json('POST', $url, [
+            'announcement_id' => $announcement->id,
+            'content' => 'Test content',
+        ]);
+
+        $this->assertEquals(1, Comment::count());
+        $this->assertDatabaseHas(
+            app(Comment::class)->getTable(),
+            [
+                'announcement_id' => $announcement->id,
+                'content' => 'Test content',
+                'created_by' => $user->id,
+            ]
+        );
+    }
+
+    public function testCreateCommentInExistingAnnouncementWithValidDataExpectReturnCreatedComment()
+    {
+        $user = User::factory()->create();
+        $user->rawPassword = 'password';
+
+        $announcement = Announcement::factory()->create();
+
+        $url = $url = route('badaso.announcement.comment');
+        $response = AuthHelper::asUser($this, $user)->json('POST', $url, [
+            'announcement_id' => $announcement->id,
+            'content' => 'This is my comment',
+        ]);
+
+        $commentData = $response->json('data');
+
+        $response->assertStatus(200);
+        $this->assertArrayHasKey('id', $commentData);
+        $this->assertEquals($commentData['announcementId'], $announcement->id);
+        $this->assertEquals($commentData['content'], 'This is my comment');
+        $this->assertEquals($commentData['createdBy'], $user->id);
     }
 }
