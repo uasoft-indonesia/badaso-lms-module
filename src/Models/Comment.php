22faf944f7b91d6696f686c88bda692fbcbbf44f
Nabila Khansa <nabila.khansa@gmail.com>
2022-03-28 16:14:12 +0700

    [GREEN] create comment factory, model, migration

---
+++ src/Models/Comment.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Uasoft\Badaso\Module\LMSModule\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Uasoft\Badaso\Module\LMSModule\Factories\CommentFactory;
+
+class Comment extends Model
+{
+    use HasFactory;
+
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+        $this->setTable(config('badaso.database.prefix').'comments');
+    }
+
+    protected $fillable = [
+        'announcement_id',
+        'content',
+        'created_by',
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
+
+    public function announcement()
+    {
+        return $this->belongsTo(Course::class, 'announcement_id');
+    }
+
+    public function createdBy()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    protected static function newFactory()
+    {
+        return CommentFactory::new();
+    }
+}
