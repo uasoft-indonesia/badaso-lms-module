61c51a54541ccde7351a64844f0e0958fe45b8cf
Nabila Khansa <nabila.khansa@gmail.com>
2022-03-28 16:15:29 +0700

    [GREEN] implement create comment base

--- src/Controllers/AnnouncementController.php
+++ src/Controllers/AnnouncementController.php
@@ -9,6 +9,7 @@ use Uasoft\Badaso\Controllers\Controller;
 use Uasoft\Badaso\Helpers\ApiResponse;
 use Uasoft\Badaso\Module\LMSModule\Helpers\CourseUserHelper;
 use Uasoft\Badaso\Module\LMSModule\Models\Announcement;
+use Uasoft\Badaso\Module\LMSModule\Models\Comment;
 
 class AnnouncementController extends Controller
 {
@@ -91,6 +92,27 @@ class AnnouncementController extends Controller
             $announcement->save();
 
             return ApiResponse::success($announcement->toArray());
+        } catch (Exception $e) {
+            return ApiResponse::failed($e);
+        }
+    }
+
+    public function comment(Request $request)
+    {
+        try {
+            $user = auth()->user();
+            $request->validate([
+                'announcement_id' => 'required|integer',
+                'content' => 'required|string|max:65535',
+            ]);
+
+            $comment = Comment::create([
+                'announcement_id' => $request->input('announcement_id'),
+                'content' => $request->input('content'),
+                'created_by' => $user->id,
+            ]);
+
+            return ApiResponse::success($comment->toArray());
         } catch (Exception $e) {
             return ApiResponse::failed($e);
         }
