4b50abe68a5ff6b5cb2906ebd7df18e88fd6c85b
Aldi Naufal Fitrah <aldifitrahx@gmail.com>
2022-04-14 17:02:21 +0700

    [RED] test create lesson material expect insert to database

--- tests/Feature/LessonMaterialApiTest.php
+++ tests/Feature/LessonMaterialApiTest.php
@@ -5,6 +5,7 @@ namespace Uasoft\Badaso\Module\LMSModule\Tests\Feature;
 use Tests\TestCase;
 use Uasoft\Badaso\Module\LMSModule\Enums\CourseUserRole;
 use Uasoft\Badaso\Module\LMSModule\Models\Course;
+use Uasoft\Badaso\Module\LMSModule\Models\LessonMaterial;
 use Uasoft\Badaso\Module\LMSModule\Models\User;
 use Uasoft\Badaso\Module\LMSModule\Tests\Helpers\AuthHelper;
 
@@ -64,5 +65,31 @@ class LessonMaterialApiTest extends TestCase
         ]);
 
         $response->assertStatus(400);
+    }
+
+    public function testCreateLessonMaterialAsTeacherWithValidDataExpectInsertedToDatabase()
+    {
+        $user = User::factory()->create();
+        $user->rawPassword = 'password';
+
+        $course = Course::factory()
+            ->hasAttached($user, ['role' => CourseUserRole::TEACHER])
+            ->create();
+
+        $url = route('badaso.lesson_material.add');
+        AuthHelper::asUser($this, $user)->json('POST', $url, [
+            'course_id' => $course->id,
+            'title' => 'test title',
+        ]);
+
+        $this->assertEquals(1, LessonMaterial::count());
+        $this->assertDatabaseHas(
+            app(LessonMaterial::class)->getTable(),
+            [
+                'course_id' => $course->id,
+                'title' => 'test title',
+                'created_by' => $user->id,
+            ]
+        );
     }
 }
