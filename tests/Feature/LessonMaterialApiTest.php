718dd41a45eb99a68815465557d72dbafb8e9874
Aldi Naufal Fitrah <aldifitrahx@gmail.com>
2022-04-14 16:28:55 +0700

    [RED] test create lesson material should only be done by teacher

--- tests/Feature/LessonMaterialApiTest.php
+++ tests/Feature/LessonMaterialApiTest.php
@@ -3,6 +3,8 @@
 namespace Uasoft\Badaso\Module\LMSModule\Tests\Feature;
 
 use Tests\TestCase;
+use Uasoft\Badaso\Module\LMSModule\Enums\CourseUserRole;
+use Uasoft\Badaso\Module\LMSModule\Models\Course;
 use Uasoft\Badaso\Module\LMSModule\Models\User;
 use Uasoft\Badaso\Module\LMSModule\Tests\Helpers\AuthHelper;
 
@@ -23,6 +25,24 @@ class LessonMaterialApiTest extends TestCase
         $url = route('badaso.lesson_material.add');
         $response = AuthHelper::asUser($this, $user)->json('POST', $url, [
             'course_id' => 1,
+            'title' => 'test title',
+        ]);
+
+        $response->assertStatus(400);
+    }
+
+    public function testCreateLessonMaterialAsStudentExpectResponse400()
+    {
+        $user = User::factory()->create();
+        $user->rawPassword = 'password';
+
+        $course = Course::factory()
+            ->hasAttached($user, ['role' => CourseUserRole::STUDENT])
+            ->create();
+
+        $url = route('badaso.lesson_material.add');
+        $response = AuthHelper::asUser($this, $user)->json('POST', $url, [
+            'course_id' => $course->id,
             'title' => 'test title',
         ]);
 
