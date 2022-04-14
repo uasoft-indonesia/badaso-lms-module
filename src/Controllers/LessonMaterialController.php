1b7d5ff8acf1795dafe5188e0ca17121208d0a87
Aldi Naufal Fitrah <aldifitrahx@gmail.com>
2022-04-14 14:30:53 +0700

    [GREEN] implement add lesson material endpoint skeleton

---
+++ src/Controllers/LessonMaterialController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Uasoft\Badaso\Module\LMSModule\Controllers;
+
+use Illuminate\Http\Request;
+use Uasoft\Badaso\Controllers\Controller;
+
+class LessonMaterialController extends Controller
+{
+    public function add(Request $request)
+    {
+    }
+}
