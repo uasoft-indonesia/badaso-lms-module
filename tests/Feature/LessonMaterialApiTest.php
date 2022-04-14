fdc72dffe531b658d96c9ac95ecab001656639b8
Aldi Naufal Fitrah <aldifitrahx@gmail.com>
2022-04-14 15:14:11 +0700

    [RED] test create lesson material in unenrolled course

--- tests/Feature/LessonMaterialApiTest.php
+++ tests/Feature/LessonMaterialApiTest.php
@@ -3,6 +3,8 @@
 namespace Uasoft\Badaso\Module\LMSModule\Tests\Feature;
 
 use Tests\TestCase;
+use Uasoft\Badaso\Module\LMSModule\Models\User;
+use Uasoft\Badaso\Module\LMSModule\Tests\Helpers\AuthHelper;
 
 class LessonMaterialApiTest extends TestCase
 {
@@ -11,5 +13,19 @@ class LessonMaterialApiTest extends TestCase
         $url = route('badaso.lesson_material.add');
         $response = $this->json('POST', $url);
         $response->assertStatus(401);
+    }
+
+    public function testCreateLessonMaterialInAnUnerolledCourseExpectResponse400()
+    {
+        $user = User::factory()->create();
+        $user->rawPassword = 'password';
+
+        $url = route('badaso.lesson_material.add');
+        $response = AuthHelper::asUser($this, $user)->json('POST', $url, [
+            'course_id' => 1,
+            'title' => 'test title',
+        ]);
+
+        $response->assertStatus(400);
     }
 }
