4b84956b4e95f6344ef329532df30f6dfd5c6e25
Aldi Naufal Fitrah <aldifitrahx@gmail.com>
2022-04-14 15:14:29 +0700

    [GREEN] only allow create lesson material in enrolled course

--- src/Controllers/LessonMaterialController.php
+++ src/Controllers/LessonMaterialController.php
@@ -2,12 +2,27 @@
 
 namespace Uasoft\Badaso\Module\LMSModule\Controllers;
 
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 use Uasoft\Badaso\Controllers\Controller;
+use Uasoft\Badaso\Helpers\ApiResponse;
+use Uasoft\Badaso\Module\LMSModule\Helpers\CourseUserHelper;
 
 class LessonMaterialController extends Controller
 {
     public function add(Request $request)
     {
+        try {
+            $user = auth()->user();
+
+            if (! CourseUserHelper::isUserInCourse($user->id, $request->input('course_id'))) {
+                throw ValidationException::withMessages([
+                    'course_id' => 'Course not found',
+                ]);
+            }
+        } catch (Exception $e) {
+            return ApiResponse::failed($e);
+        }
     }
 }
