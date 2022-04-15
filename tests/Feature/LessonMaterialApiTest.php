0911dfa636fd63ea06852946b45828a360f18d48
Aldi Naufal Fitrah <aldifitrahx@gmail.com>
2022-04-15 20:25:32 +0700

    [RED] test read lesson material shuold response needed field

--- tests/Feature/LessonMaterialApiTest.php
+++ tests/Feature/LessonMaterialApiTest.php
@@ -6,6 +6,7 @@ use Tests\TestCase;
 use Uasoft\Badaso\Module\LMSModule\Enums\CourseUserRole;
 use Uasoft\Badaso\Module\LMSModule\Models\Course;
 use Uasoft\Badaso\Module\LMSModule\Models\LessonMaterial;
+use Uasoft\Badaso\Module\LMSModule\Models\Topic;
 use Uasoft\Badaso\Module\LMSModule\Models\User;
 use Uasoft\Badaso\Module\LMSModule\Tests\Helpers\AuthHelper;
 
@@ -117,14 +118,14 @@ class LessonMaterialApiTest extends TestCase
         $this->assertEquals($lesssonMaterialData['createdBy'], $user->id);
     }
 
-    public function testReadLessonMaterialsWithoutLoginExpectResponse401()
+    public function testReadLessonMaterialWithoutLoginExpectResponse401()
     {
         $url = route('badaso.lesson_material.read', ['id' => 1]);
         $response = $this->json('GET', $url);
         $response->assertStatus(401);
     }
 
-    public function testReadLessonMaterialsGivenUnenrolledCourseExpectResponse400()
+    public function testReadLessonMaterialGivenUnenrolledCourseExpectResponse400()
     {
         $user = User::factory()->create();
         $user->rawPassword = 'password';
@@ -135,5 +136,36 @@ class LessonMaterialApiTest extends TestCase
         $response = AuthHelper::asUser($this, $user)->json('GET', $url);
 
         $response->assertStatus(400);
+    }
+
+    public function testReadLessonMaterialGivenValidDataExpectRespondsWithLessonMaterial()
+    {
+        $user = User::factory()->create();
+        $user->rawPassword = 'password';
+
+        $course = Course::factory()
+            ->hasAttached($user, ['role' => CourseUserRole::STUDENT])
+            ->create();
+
+        $topic = Topic::factory()
+            ->for($course)
+            ->create();
+
+        $lessonMaterial = LessonMaterial::factory()
+            ->for($course)
+            ->for($topic)
+            ->create();
+
+        $url = route('badaso.lesson_material.read', ['id' => $lessonMaterial->id]);
+        $response = AuthHelper::asUser($this, $user)->json('GET', $url);
+
+        $lessonMaterialData = $response->json('data');
+
+        $response->assertStatus(200);
+        $this->assertArrayHasKey('id', $lessonMaterialData);
+        $this->assertEquals($lessonMaterialData['courseId'], $course->id);
+        $this->assertEquals($lessonMaterialData['title'], $lessonMaterial->title);
+        $this->assertEquals($lessonMaterialData['createdBy']['name'], $lessonMaterial->createdBy->name);
+        $this->assertEquals($lessonMaterialData['topic']['title'], $lessonMaterial->topic->title);
     }
 }
