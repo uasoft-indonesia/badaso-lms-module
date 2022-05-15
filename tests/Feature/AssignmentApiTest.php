a3a7bcb5331e2b43d2fd9b6a2657888d4c932ac5
Aldi Naufal Fitrah <aldifitrahx@gmail.com>
2022-05-15 13:41:42 +0700

    [RED] test create assignment in unenrolled course

--- tests/Feature/AssignmentApiTest.php
+++ tests/Feature/AssignmentApiTest.php
@@ -3,6 +3,8 @@
 namespace Uasoft\Badaso\Module\LMSModule\Tests\Feature;
 
 use Tests\TestCase;
+use Uasoft\Badaso\Module\LMSModule\Models\User;
+use Uasoft\Badaso\Module\LMSModule\Tests\Helpers\AuthHelper;
 
 class AssignmentApiTest extends TestCase
 {
@@ -11,5 +13,20 @@ class AssignmentApiTest extends TestCase
         $url = route('badaso.assignment.add');
         $response = $this->json('POST', $url);
         $response->assertStatus(401);
+    }
+
+    public function testCreateAssignmentInAnUnerolledCourseExpectResponse400()
+    {
+        $user = User::factory()->create();
+        $user->rawPassword = 'password';
+
+        $url = route('badaso.assignment.add');
+        $response = AuthHelper::asUser($this, $user)->json('POST', $url, [
+            'course_id' => 1,
+            'title' => 'test title',
+            'due_date' => '2022-05-24T23:55:00+07:00',
+        ]);
+
+        $response->assertStatus(400);
     }
 }
