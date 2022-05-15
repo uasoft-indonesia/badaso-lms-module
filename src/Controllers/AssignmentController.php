f07c50f4d88814be7d8953b9379a663bb96e3442
Aldi Naufal Fitrah <aldifitrahx@gmail.com>
2022-05-15 13:06:13 +0700

    [GREEN] create assignment routes and controller skeleton

---
+++ src/Controllers/AssignmentController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Uasoft\Badaso\Module\LMSModule\Controllers;
+
+use Illuminate\Http\Request;
+use Uasoft\Badaso\Controllers\Controller;
+
+class AssignmentController extends Controller
+{
+    public function add(Request $request)
+    {
+    }
+}
