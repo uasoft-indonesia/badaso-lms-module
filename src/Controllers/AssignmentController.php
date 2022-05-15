955efab8cbe1e67964dd0bbecfebadff0b95c1d5
Aldi Naufal Fitrah <aldifitrahx@gmail.com>
2022-05-15 22:29:27 +0700

    [CHORE] fix style ci

--- src/Controllers/AssignmentController.php
+++ src/Controllers/AssignmentController.php
@@ -66,7 +66,7 @@ class AssignmentController extends Controller
                 'topic:id,title',
             ])->find($id);
 
-            if (!CourseUserHelper::isUserInCourse(
+            if (! CourseUserHelper::isUserInCourse(
                 $user->id,
                 $assignment?->course_id,
             )) {
