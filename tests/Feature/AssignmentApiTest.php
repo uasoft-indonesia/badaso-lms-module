2fb4fb056b419030d9bd61104372887448bc2449
Aldi Naufal Fitrah <aldifitrahx@gmail.com>
2022-05-15 13:43:48 +0700

    [RED] test create assignment as a student

--- tests/Feature/AssignmentApiTest.php
+++ tests/Feature/AssignmentApiTest.php
@@ -3,6 +3,8 @@
 namespace Uasoft\Badaso\Module\LMSModule\Tests\Feature;
 
 use Tests\TestCase;
+use Uasoft\Badaso\Module\LMSModule\Enums\CourseUserRole;
+use Uasoft\Badaso\Module\LMSModule\Models\Course;
 use Uasoft\Badaso\Module\LMSModule\Models\User;
 use Uasoft\Badaso\Module\LMSModule\Tests\Helpers\AuthHelper;
 
@@ -23,6 +25,25 @@ class AssignmentApiTest extends TestCase
         $url = route('badaso.assignment.add');
         $response = AuthHelper::asUser($this, $user)->json('POST', $url, [
             'course_id' => 1,
+            'title' => 'test title',
+            'due_date' => '2022-05-24T23:55:00+07:00',
+        ]);
+
+        $response->assertStatus(400);
+    }
+
+    public function testCreateAssignmentAsStudentExpectResponse400()
+    {
+        $user = User::factory()->create();
+        $user->rawPassword = 'password';
+
+        $course = Course::factory()
+            ->hasAttached($user, ['role' => CourseUserRole::STUDENT])
+            ->create();
+
+        $url = route('badaso.assignment.add');
+        $response = AuthHelper::asUser($this, $user)->json('POST', $url, [
+            'course_id' => $course->id,
             'title' => 'test title',
             'due_date' => '2022-05-24T23:55:00+07:00',
         ]);
