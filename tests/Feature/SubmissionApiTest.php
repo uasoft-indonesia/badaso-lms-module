e92411aa0a2d51ebd4019d2c82328ec160a3f63e
nofamex <nsiren517@gmail.com>
2022-05-17 23:41:26 +0700

    [GREEN] Create Submission Test

--- tests/Feature/SubmissionApiTest.php
+++ tests/Feature/SubmissionApiTest.php
@@ -3,7 +3,123 @@
 namespace Uasoft\Badaso\Module\LMSModule\Tests\Feature;
 
 use Tests\TestCase;
+use Uasoft\Badaso\Module\LMSModule\Enums\CourseUserRole;
+use Uasoft\Badaso\Module\LMSModule\Models\Assignment;
+use Uasoft\Badaso\Module\LMSModule\Models\Course;
+use Uasoft\Badaso\Module\LMSModule\Models\Submission;
+use Uasoft\Badaso\Module\LMSModule\Models\User;
+use Uasoft\Badaso\Module\LMSModule\Tests\Helpers\AuthHelper;
 
 class SubmissionApiTest extends TestCase
 {
+    public function testSubmissionWithoutLogin()
+    {
+        $url = route('badaso.submission.add');
+        $response = $this->json('POST', $url);
+        $response->assertStatus(401);
+    }
+
+    public function testSubmissionWithoutAsssignment()
+    {
+        $url = route('badaso.submission.add');
+        $user = User::factory()->create();
+        $user->rawPassword = 'password';
+
+        $response = AuthHelper::asUser($this, $user)->json('POST', $url, [
+            'assignment_id' => 1,
+        ]);
+
+        $response->assertStatus(400);
+    }
+
+    public function testSubmissionWithoutUserInCourse()
+    {
+        $url = route('badaso.submission.add');
+
+        $user = User::factory()->create();
+        $user->rawPassword = 'password';
+
+        $assignment = Assignment::factory()
+            ->create();
+
+        $response = AuthHelper::asUser($this, $user)->json('POST', $url, [
+            'assignment_id' => $assignment->id,
+        ]);
+
+        $response->assertStatus(400);
+    }
+
+    public function testSubmissionPassDueDate()
+    {
+        $url = route('badaso.submission.add');
+
+        $user = User::factory()->create();
+        $user->rawPassword = 'password';
+
+        $course = Course::factory()
+            ->hasAttached($user, ['role' => CourseUserRole::STUDENT])
+            ->create();
+
+        $assignment = Assignment::factory()
+            ->for($course)
+            ->create([
+                'due_date' => '2022-05-10 23:55:00+07:00',
+            ]);
+
+        $response = AuthHelper::asUser($this, $user)->json('POST', $url, [
+            'assignment_id' => $assignment->id,
+        ]);
+
+        $response->assertStatus(400);
+    }
+
+    public function testSubmissionWithExistingSubmission()
+    {
+        $url = route('badaso.submission.add');
+
+        $user = User::factory()->create();
+        $user->rawPassword = 'password';
+
+        $course = Course::factory()
+            ->hasAttached($user, ['role' => CourseUserRole::STUDENT])
+            ->create();
+
+        $assignment = Assignment::factory()
+            ->for($course)
+            ->create();
+        
+        Submission::factory()
+            ->for($user)
+            ->create();
+
+        $response = AuthHelper::asUser($this, $user)->json('POST', $url, [
+            'assignment_id' => $assignment->id,
+        ]);
+
+        $response->assertStatus(400);
+    }
+
+    public function testSubmissionSuccess()
+    {
+        $url = route('badaso.submission.add');
+
+        $user = User::factory()->create();
+        $user->rawPassword = 'password';
+
+        $course = Course::factory()
+            ->hasAttached($user, ['role' => CourseUserRole::STUDENT])
+            ->create();
+
+        $assignment = Assignment::factory()
+            ->for($course)
+            ->create([
+                'due_date' => '2122-05-10 23:55:00+07:00',
+            ]);
+        
+        AuthHelper::asUser($this, $user)->json('POST', $url, [
+            'assignment_id' => $assignment->id,
+        ]);
+
+        $this->assertEquals(1, Submission::count());
+    }
 }
