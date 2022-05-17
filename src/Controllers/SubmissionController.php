26e4217a3d14a05ddec7a2c4c644b4669454822a
nofamex <nsiren517@gmail.com>
2022-05-18 00:54:48 +0700

    [RED] Edit Submission

--- src/Controllers/SubmissionController.php
+++ src/Controllers/SubmissionController.php
@@ -113,4 +113,57 @@ class SubmissionController extends Controller
 
         return ApiRespOnse::success($response);
     }
+
+    public function edit(Request $request, $id)
+    {
+        try {
+            $user = auth()->user();
+
+            $request->validate([
+                'file_url' => 'nullable|string|max:65535',
+                'link_url' => 'nullable|string|max:65535',
+            ]);
+
+            $submission = Submission::with([
+                'assignment:id,course_id,due_date',
+            ])->where([
+                ['id', '=', $id],
+                ['user_id', '=', $user->id],
+            ])->first();
+
+            if (! $submission) {
+                throw ValidationException::withMessages([
+                    'id' => 'Submission not found',
+                ]);;
+            }
+
+            if (! CourseUserHelper::isUserInCourse(
+                $user->id,
+                $submission->assignment->course_id,
+            )) {
+                throw ValidationException::withMessages([
+                    'id' => 'Must enroll the course to submit assignment',
+                ]);
+            }
+
+            $due_date = strtotime($submission->assignment->due_date);
+            $current_date = strtotime(date('Y-m-d H:i:sP'));
+
+            // Check if submission pass due date
+            if ($current_date > $due_date) {
+                throw ValidationException::withMessages([
+                    'date' => 'You have passed submission due date',
+                ]);
+            }
+
+            $submission->fill($request->only([
+                'file_url',
+                'link_url',
+            ]))->save();
+
+            return ApiRespOnse::success($submission->toArray());
+        } catch (Exception $e) {
+            return ApiResponse::failed($e);
+        }
+    }
 }
--- src/Routes/api.php
+++ src/Routes/api.php
@@ -157,6 +157,10 @@ Route::group(['prefix' => $api_route_prefix, 'as' => 'badaso.', 'middleware' => 
             Route::get('/{id}', HelpersRoute::getController('SubmissionController@read'))
                 ->middleware(BadasoAuthenticate::class)
                 ->name('read');
+            
+            Route::put('/{id}', HelpersRoute::getController('SubmissionController@edit'))
+                ->middleware(BadasoAuthenticate::class)
+                ->name('edit');
         });
     });
 });
