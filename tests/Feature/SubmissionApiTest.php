69d1387fdf3bbce880ce10834dc138d25f9e0e0d
nofamex <nsiren517@gmail.com>
2022-05-18 01:16:22 +0700

    [GREEN] Edit Submission

--- tests/Feature/SubmissionApiTest.php
+++ tests/Feature/SubmissionApiTest.php
@@ -175,4 +175,98 @@ class SubmissionApiTest extends TestCase
         $response->assertStatus(200);
         $this->assertEquals($data['status'], 'submitted');
     }
+
+    public function testEditSubmissionWithoutLogin()
+    {
+        $url = route('badaso.submission.edit', ['id' => 1]);
+        $response = $this->json('GET', $url);
+        $response->assertStatus(401);
+    }
+
+    public function testEditSubmissionWithoutExisting()
+    {
+        $url = route('badaso.submission.edit', ['id' => 1]);
+        $user = User::factory()->create();
+        $user->rawPassword = 'password';
+
+        $response = AuthHelper::asUser($this, $user)->json('PUT', $url);
+        $response->assertStatus(400);
+    }
+
+    public function testEditSubmissionWithoutUserInCourse()
+    {
+        $user = User::factory()->create();
+        $user->rawPassword = 'password';
+
+        $submission = Submission::factory()
+            ->for($user)
+            ->create();
+
+        $url = route('badaso.submission.edit', ['id' => $submission->id]);
+
+        $response = AuthHelper::asUser($this, $user)->json('PUT', $url);
+
+        $response->assertStatus(400);
+    }
+
+    public function testEditSubmissionPassDueDate()
+    {
+        $user = User::factory()->create();
+        $user->rawPassword = 'password';
+
+        $course = Course::factory()
+            ->hasAttached($user, ['role' => CourseUserRole::STUDENT])
+            ->create();
+
+        $assignment = Assignment::factory()
+            ->for($course)
+            ->create([
+                'due_date' => '2020-05-10 23:55:00+07:00',
+            ]);
+
+        $submission = Submission::factory()
+            ->for($assignment)
+            ->for($user)
+            ->create();
+        
+        $url = route('badaso.submission.edit', ['id' => $submission->id]);
+
+        $response = AuthHelper::asUser($this, $user)->json('PUT', $url, [
+            'file_url' => 'www.google.com',
+        ]);
+
+        $response->assertStatus(400);
+    }
+
+    public function testEditSubmissionSuccess()
+    {
+        $user = User::factory()->create();
+        $user->rawPassword = 'password';
+
+        $course = Course::factory()
+            ->hasAttached($user, ['role' => CourseUserRole::STUDENT])
+            ->create();
+
+        $assignment = Assignment::factory()
+            ->for($course)
+            ->create([
+                'due_date' => '2122-05-10 23:55:00+07:00',
+            ]);
+
+        $submission = Submission::factory()
+            ->for($assignment)
+            ->for($user)
+            ->create();
+        
+        $url = route('badaso.submission.edit', ['id' => $submission->id]);
+
+        $response = AuthHelper::asUser($this, $user)->json('PUT', $url, [
+            'file_url' => 'www.google.com',
+        ]);
+
+        $data = $response->json('data');
+
+        $response->assertStatus(200);
+        $this->assertEquals($data['fileUrl'], 'www.google.com');
+    }
 }
